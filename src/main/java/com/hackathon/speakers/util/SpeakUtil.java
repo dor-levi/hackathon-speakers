54b6d536ea161434ded9b340c46c9c41dc66c1d8
Dor Levi <dor.levi@takipi.com>
2016-12-15 14:41:05 +0200

    Switch to festival

--- src/main/java/com/hackathon/speakers/util/SpeakUtil.java
+++ src/main/java/com/hackathon/speakers/util/SpeakUtil.java
@@ -5,15 +5,28 @@ import org.slf4j.LoggerFactory;
 
 public class SpeakUtil {
     private static final Logger logger = LoggerFactory.getLogger(SpeakUtil.class);
+    private static final String FESTIVAL = "echo \"%s\" | festival --tts";
     private static final String ESPEAK = "espeak -ven+f3 -k5 -s150 \"%s\"";
 
     public static void speak(String text) {
-        espeak(text);
+        festival(text);
     }
 
+    private static void festival(String text) {
+        try {
+            String command = String.format(FESTIVAL, text);
+            logger.info(command);
+            Runtime.getRuntime().exec(command);
+        } catch (Exception e) {
+            logger.error("Failed to espeak.", e);
+        }
+    }
+    
     private static void espeak(String text) {
         try {
-            Runtime.getRuntime().exec(String.format(ESPEAK, text));
+            String command = String.format(ESPEAK, text);
+            logger.info(command);
+            Runtime.getRuntime().exec(command);
         } catch (Exception e) {
             logger.error("Failed to espeak.", e);
         }
