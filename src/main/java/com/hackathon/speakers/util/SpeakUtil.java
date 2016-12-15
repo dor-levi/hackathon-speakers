78754432f3e55ecb8c1db120efd92e8a7b83d19b
Dor Levi <dor.levi@takipi.com>
2016-12-15 14:35:26 +0200

    Intro espeak

--- src/main/java/com/hackathon/speakers/util/SpeakUtil.java
+++ src/main/java/com/hackathon/speakers/util/SpeakUtil.java
@@ -5,8 +5,17 @@ import org.slf4j.LoggerFactory;
 
 public class SpeakUtil {
     private static final Logger logger = LoggerFactory.getLogger(SpeakUtil.class);
-    
+    private static final String ESPEAK = "espeak -ven+f3 -k5 -s150 \"%s\"";
+
     public static void speak(String text) {
-        logger.info(text);
+        espeak(text);
+    }
+
+    private static void espeak(String text) {
+        try {
+            Runtime.getRuntime().exec(String.format(ESPEAK, text));
+        } catch (Exception e) {
+            logger.error("Failed to espeak.", e);
+        }
     }
 }
