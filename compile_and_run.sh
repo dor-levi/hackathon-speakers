4f7764e38c5450e77225689791af287e02bb6a28
Dor Levi <dor.levi@takipi.com>
2016-12-15 15:38:57 +0200

    Up version to 1.0.0

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -4,4 +4,4 @@ sudo git pull
 
 sudo ~/maven/bin/mvn package
 
-java -jar target/hackathons-speakers-0.0.1.jar server settings.yml
+java -jar target/hackathons-speakers-1.0.0.jar server settings.yml
